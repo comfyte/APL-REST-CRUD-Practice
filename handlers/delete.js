8308a69086653d549a2c8d16f4f46d0534b15742
Phynopie <69754690+Phynopie@users.noreply.github.com>
2021-05-03 07:00:11 +0700

    Implemented DELETE and POST

--- handlers/delete.js
+++ handlers/delete.js
@@ -7,5 +7,19 @@ import { Karyawan } from "../data-model";
  * @param {import("next").NextApiResponse} response 
  */
 export function deleteHandler(request, response) {
+    if (request.headers["content-type"] !== "application/json") {
+        response.status(415).end();
+        return;
+    }
+
+    if (!request.query || !request.query.empId) {
+        response.status(400).end();
+        return;
+    }
+    
+    const existingData = readData();
+ 
+    const newData = existingData.filter((x) => x.id_karyawan !== request.query.empId);
+    writeData(newData);
     response.status(200).end();
 }
--- handlers/post.js
+++ handlers/post.js
@@ -6,6 +6,31 @@ import { Karyawan } from "../data-model";
  * @param {import("next").NextApiRequest} request
  * @param {import("next").NextApiResponse} response 
  */
-export function postHandler(request, response) {
+ export function postHandler(request, response) {
+    if (request.headers["content-type"] !== "application/json") {
+        response.status(415).end();
+        return;
+    }
+
+    if (!request.query || !request.query.empId) {
+        response.status(400).end();
+        return;
+    }
+
+    const {id_karyawan, nama, posisi } = request.body;
+    if (!id_karyawan || !nama || !posisi) {
+        response.status(400).end();
+        return;
+    }
+
+    const existingData = readData();
+
+    if(existingData.filter(emp => emp.id_karyawan === id_karyawan).length > 0) {
+        response.status(400).end();
+        return; 
+    }
+
+    existingData.push(new Karyawan(id_karyawan,nama,posisi));
+    writeData(existingData);
     response.status(200).end();
 }
